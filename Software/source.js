6179cfe1400e68fa337cc4377060b6e3aadccfd1
tfraser <18135@student.bbc.qld.edu.au>
2017-07-19 11:51:08 +1000

    Changes to html ui

--- Software/source.js
+++ Software/source.js
@@ -1,7 +1,7 @@
 function loginPressed(){
     document.getElementById("sidebar").style.width = "85%";
     document.getElementById("loginButton").style.visibility = "hidden";
-
+    document.getElementById("loginBar").style.marginRight = "-27.5%";
 }
 
 function loaded(){
