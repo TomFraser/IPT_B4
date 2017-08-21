1ee768c8726c46166a2415ba95c948b6dadfa4b4
tfraser <18135@student.bbc.qld.edu.au>
2017-08-21 14:14:24 +1000

    YaYeet

---
+++ Software/general.py
@@ -0,0 +1,3 @@
+def sendUser(email, password):
+    print("Sending User Data to BBC")
+    #Send user info to BBC
--- Software/main.py
+++ Software/main.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request, session, redirect, url_for
+from general import sendUser
 import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def splash():
-    return render_template("index.html")
+    email = request.form["email"]
+    password = request.form["password"]
+    sendUser(email, password)
+    return render_template("login.html")
+
+@app.route('/home', methods=['GET', 'POST'])
+def landingPage():
+
 
 if __name__ == "__main__":
     app.run(debug=True)
