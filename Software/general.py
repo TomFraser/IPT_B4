b320a1745d5434a05f9bcba69fca4c06aaaa97f0
tfraser <18135@student.bbc.qld.edu.au>
2017-08-22 10:00:43 +1000

    Started work on the flask part of the application. You can now log in an out using a username and password

--- Software/general.py
+++ Software/general.py
@@ -1,3 +1,9 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+debug = False
 def sendUser(email, password):
-    print("Sending User Data to BBC")
+    if debug:
+        print(email)
+        print(password)
+        print("Sending User Data to BBC")
     #Send user info to BBC
+    return True
--- Software/main.py
+++ Software/main.py
@@ -6,13 +6,21 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def splash():
-    email = request.form["email"]
-    password = request.form["password"]
-    sendUser(email, password)
-    return render_template("login.html")
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        if(sendUser(email, password)) == True:
+            return redirect("/home")
+    elif request.method == 'GET':
+        return render_template("login.html")
 
 @app.route('/home', methods=['GET', 'POST'])
 def landingPage():
+    if request.method == "POST":
+        if request.form['sidemenuButton'] == 'logout':
+            return redirect("/")
+    elif request.method == "GET":
+        return render_template("landingPage.html")
 
 
 if __name__ == "__main__":
